f46a1ce431f92d9c50be3fd24fc85b1dfe83d799
Peter Stadler <stadlerpeter@yahoo.fr>
2015-06-02 17:31:55 +0200

    added some javascript for input forms

--- resources/js/smufl-scripts.js
+++ resources/js/smufl-scripts.js
@@ -1,22 +1,31 @@
 /*
- * Some Javscript functions for SMuFL-Browser
+ * Some Javascript functions for SMuFL-Browser
  */
 
-function gotoPage(page) {
-/*    var data = $("#fun-query-form").serialize();*/
-    /*$.ajax({
-	    type: "POST",
-	    url: "ajax.html",
-	    data: data + "&action=search",
-	    success: function (data) {
-            $("#results").fadeOut(100, function() {
-                $(this).html(data);
-                $(this).fadeIn(100, function() {
-                    $(".signature").highlight();
-                });
-                timeout = null;
-            });
-	    }
-    });*/
-    console.log(page);
-}
+$.fn.facets = function ()
+{
+     this.selectize({
+        plugins: ['remove_button'],
+        hideSelected: true,
+        onDropdownClose: function(e){
+            var params = [];
+            /* Set filters */
+            $('option:selected').each(function() {
+                var facet = $(this).parent().attr('name');
+                var value = $(this).attr('value');
+                /*console.log(facet + '=' + value);*/
+                params.push(facet + '=' + encodeURI(value))
+            })
+            
+            /* Refresh page */
+            self.location='?'+params.join('&')
+        }
+    })
+};
+
+/* Initialize selectize plugin for the input forms */
+$(document).ready(function()  {
+    $("#ranges-list").facets();
+    $("#glyphnames-list").facets();
+    $("#classes-list").facets();
+});
