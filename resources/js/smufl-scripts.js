3b3dd747f20176e68e915915208f6a76e78ab05d
Peter Stadler <stadlerpeter@yahoo.fr>
2020-04-07 14:35:00 +0200

    replace selectize plugin with select2

--- resources/js/smufl-scripts.js
+++ resources/js/smufl-scripts.js
@@ -2,30 +2,7 @@
  * Some Javascript functions for SMuFL-Browser
  */
 
-$.fn.facets = function ()
-{
-     this.selectize({
-        plugins: ['remove_button'],
-        hideSelected: true,
-        onDropdownClose: function(e){
-            var params = [];
-            /* Set filters */
-            $('option:selected').each(function() {
-                var facet = $(this).parent().attr('name');
-                var value = $(this).attr('value');
-                /*console.log(facet + '=' + value);*/
-                params.push(facet + '=' + encodeURI(value))
-            })
-            
-            /* Refresh page */
-            self.location='?'+params.join('&')
-        }
-    })
-};
-
-/* Initialize selectize plugin for the input forms */
-$(document).ready(function()  {
-    $("#ranges-list").facets();
-    $("#glyphnames-list").facets();
-    $("#classes-list").facets();
+/* Initialize select2 plugin for the input forms */
+$('select').select2({
+    closeOnSelect: false
 });
